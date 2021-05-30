b5cc3375e2549026136eecf82687dea10d9059a6
souzavini <viniciuscarr@hotmail.com>
2021-05-29 23:45:29 -0300

    Implementacao do botao play e pause

--- App.js
+++ App.js
@@ -1,23 +1,57 @@
-import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, FlatList, StatusBar } from 'react-native';
-import CardQuote from './components/card/card.component';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  FlatList,
+  StatusBar,
+} from "react-native";
+import CardQuote from "./components/card/card.component";
+import { Button } from "react-native-paper";
 
 const notList = [
-  { id: 1, task: 'Suco de gratidão + clorofila', background: 'https://image.freepik.com/free-vector/flat-night-sky-background_23-2148032671.jpg'},
-  { id: 2, task: 'Aplaudir o por do sol', background: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTevLH9vqcGBf4kYYXN1sEafET9xBaEjxUOMg&usqp=CAU'},
-  { id: 3, task: '5 séries de namastê', background: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQa6OUSY2144YwI6mFLlxCKdyvkmKn6yLEoLA&usqp=CAU'},
-]
+  {
+    id: 1,
+    task: "Suco de gratidão + clorofila",
+    background:
+      "https://image.freepik.com/free-vector/flat-night-sky-background_23-2148032671.jpg",
+  },
+  {
+    id: 2,
+    task: "Aplaudir o por do sol",
+    background:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTevLH9vqcGBf4kYYXN1sEafET9xBaEjxUOMg&usqp=CAU",
+  },
+  {
+    id: 3,
+    task: "5 séries de namastê",
+    background:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQa6OUSY2144YwI6mFLlxCKdyvkmKn6yLEoLA&usqp=CAU",
+  },
+];
+
+
+function HandlePlayButton() {
+  SetplayButton(true);
+  console.log("Apertei");
+}
 
 export default function App() {
-  return (
+  const [blackMode, SetBlackMode] = useState(false);
 
+  return (
+    
     <SafeAreaView style={styles.container}>
-      <StatusBar
-        animated={true}
-        backgroundColor="#c64242"
-       />{notList.map(item => <CardQuote task={item.task}
-       background={item.background} key={item.id}/>)}
-          
+       
+      <StatusBar animated={true} backgroundColor="#c64242" />
+      {notList.map((item) => (
+        <CardQuote
+          task={item.task}
+          background={item.background}
+          key={item.id}
+        />
+      ))}
     </SafeAreaView>
   );
 }
@@ -25,8 +59,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#EBF5F7',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#EBF5F7",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  containerBlack: {
+    flex: 1,
+    backgroundColor: "#black",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
 });
--- components/card/card.component.jsx
+++ components/card/card.component.jsx
@@ -1,41 +1,72 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
-import { Card, Paragraph } from 'react-native-paper';
-
+import React, { useState } from "react";
+import { StyleSheet } from "react-native";
+import { Card, Paragraph, Button } from "react-native-paper";
 
 const styles = StyleSheet.create({
-    card: {
-        backgroundColor: '#EBF5F7',
-        margin: 10,
-        borderRadius: 20,
-        flex: 1,
-        width: '50vh',
-        overflow: 'hidden',
-    },
-    content: {
-        backgroundColor: '#FAE8E2',
-    },
-    cover: {
-        height: 220,
-    },
+  card: {
+    backgroundColor: "#f9e7e1",
+    margin: 10,
+    borderRadius: 20,
+    flex: 1,
+    width: "50vh",
+    overflow: "hidden",
+  },
+  content: {
+    backgroundColor: "#FAE8E2",
+  },
+  cover: {
+    height: 220,
+  },
+  action: {
+    justifyContent: "center",
+  },
+  paragraph: {
+    fontSize: 18,
+    color: "#432f2a",
+    fontWeight: 800,
+  },
 });
 
-const CardQuote = ({task,background}) => {
+const CardQuote = ({ task, background }) => {
+  const [playButton, SetplayButton] = useState(false);
 
-    return (
-        <Card style={styles.card}>
-            <Card.Cover
-                style={styles.cover}
-                resizeMode={`cover`}
-                source={{ uri: background}} />
-            <Card.Content style={styles.content}>
-            </Card.Content>
-            <p>BOTAO AQUI </p><br/>
-            <Card.Actions>
-                <Paragraph>{task}</Paragraph>
-            </Card.Actions>
-        </Card>
-    )
-}
+  function HandlePlayButton() {
+    SetplayButton(true);
+    console.log("Apertei");
+  }
+
+  function HandlePauseButton(){
+    SetplayButton(false);
+  }
+
+  return (
+    <Card style={styles.card}>
+      <Card.Cover
+        style={styles.cover}
+        resizeMode={`cover`}
+        source={{ uri: background }}
+      />
+      <Card.Content style={styles.content}></Card.Content>
+      {playButton ? (
+        <Button
+          icon="drag-vertical-variant"
+          color="#4c1318"
+          onPress={HandlePauseButton}
+          labelStyle={{ fontSize: 80 }}
+        />
+      ) : (
+        <Button
+          icon="arrow-right-drop-circle-outline"
+          onPress={HandlePlayButton}
+          color="#4c1318"
+          labelStyle={{ fontSize: 80 }}
+        />
+      )}
+      <Card.Actions style={styles.action}>
+        <Paragraph style={styles.paragraph}>{task}</Paragraph>
+      </Card.Actions>
+    </Card>
+  );
+};
 
 export default CardQuote;
